337771d5e074beb27794fa31e08f480abd382a1f
xieziwei99 <xieziwei@bupt.edu.cn>
2020-06-20 17:08:03 +0800

    配置国际化

---
+++ springboot2-2-webcrud/src/main/java/com/xzw/webcrud/config/MyMvcConfig.java
@@ -0,0 +1,38 @@
+package com.xzw.webcrud.config;
+
+import com.xzw.webcrud.component.MyLocaleResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author xieziwei99
+ * 2020-06-20
+ */
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName("login");
+                registry.addViewController("/index").setViewName("login");
+                registry.addViewController("/index.html").setViewName("login");
+            }
+        };
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new MyLocaleResolver();
+    }
+}
