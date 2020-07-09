bcda8658740ca0025a6a4f93d24f6cdb7a7037b9
xieziwei99 <xieziwei@bupt.edu.cn>
2020-07-09 14:42:02 +0800

    SpringMVC 支持 Servlet 原生 API

--- springmvc-1-HelloWorld/src/com/xzw/helloworld/HelloWorld.java
+++ springmvc-1-HelloWorld/src/com/xzw/helloworld/HelloWorld.java
@@ -4,6 +4,9 @@ import com.xzw.helloworld.entity.Product;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * @author xieziwei99
  * 2020-07-05
@@ -44,6 +47,29 @@ public class HelloWorld {
     @RequestMapping("/testPojo")
     public String testPojo(Product product) {
         System.out.println(product);
+        return "success";
+    }
+
+    /*
+    支持 Servlet 原生 API
+        1. HttpServletRequest
+        2. HttpServletResponse
+        3. HttpSession
+        4. java.security.Principal
+        5. Locale
+        6. InputStream
+        7. OutputStream
+        8. Reader
+        9. Writer
+     */
+    @RequestMapping("/testServletApi")
+    public String testServletApi(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println(request);
+        System.out.println(request.getMethod());    // GET
+        System.out.println(request.getRequestURI());    // /springmvc1/testServletApi
+        System.out.println(request.getServletPath());   // /testServletApi
+        System.out.println(response);
+        System.out.println(response.getStatus());   // 200
         return "success";
     }
 }
